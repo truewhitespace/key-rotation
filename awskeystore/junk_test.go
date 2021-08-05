5fc4ed59f3bc51f7b666ceaf55fecab1a93e86ce
Mark Eschbach <mark.eschbach@truewhitespace.com>
2021-08-05 13:52:44 -0700

    junk/stirngslice: erroroneous error message :-)

--- awskeystore/junk_test.go
+++ awskeystore/junk_test.go
@@ -2,13 +2,23 @@ package awskeystore
 
 import "testing"
 
+func (l StringSlice) assertContains(t *testing.T, expected string) {
+	if !l.Contains(expected) {
+		t.Helper()
+		t.Errorf("Expected %+q to contain %q but did not", l, expected)
+	}
+}
+
+func (l StringSlice) assertMissing(t *testing.T, expectedMissing string) {
+	if l.Contains(expectedMissing) {
+		t.Helper()
+		t.Errorf("Expected %+q not contain %q but did", l, expectedMissing)
+	}
+}
+
 func TestStringSlice_Contains(t *testing.T) {
 	slice := StringSlice{"test", "feeling", "in", "the", "way"}
-	if !slice.Contains("feeling") {
-		t.Errorf("expected to contain 'feelings' but got false")
-	}
 
-	if slice.Contains("sometimes") {
-		t.Errorf("claims 'sometimes' exists within the slice but has no such element")
-	}
+	slice.assertContains(t, "feeling")
+	slice.assertMissing(t, "sometimes")
 }
